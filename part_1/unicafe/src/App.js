5e82fa67fdd57b64cb879f06e57a094a50f6740d
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-07-27 17:34:36 +0300

    ex. 1.8

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -14,13 +14,45 @@ const Button = (props) => {
   )
 }
 
-//component for displaying stats
-const Statistics = (props) => {
+//component for single lines of stats
+const StatsLine = (props) => {
   return(
-    <div>{props.text}{props.stat}</div>
-
+  <div>{props.text}{props.stat}</div>
   )
+}
 
+//component for displaying all stats
+const Statistics = (props) => {
+  console.log(props.responses.length)
+ 
+  //stats functions (idk if JS has these built in??)
+  const sum = (arr) => {
+    return arr.reduce((total, current) => {
+      return total + current;
+    }, 0);
+  }
+  const percentPositive = (arr) => {
+    return(
+      100*(arr.filter(entry => entry === 1).length/arr.length) + ' %'
+      )
+  }
+  
+  //check whether there are stats to display, only attempt to render if yes
+  if (props.responses.length === 0) {
+    return(
+      <div>No feedback given</div>
+    )
+  }
+  return(
+    <div>
+      <StatsLine text='good ' stat={props.responses.filter(entry => entry === 1).length}/>
+      <StatsLine text='neutral ' stat={props.responses.filter(entry => entry === 0).length}/>
+      <StatsLine text='bad ' stat={props.responses.filter(entry => entry === -1).length} />
+      <StatsLine text='all ' stat={props.responses.length} />
+      <StatsLine text='average ' stat={sum(props.responses)/props.responses.length} />
+      <StatsLine text='positive ' stat={percentPositive(props.responses)} />
+    </div>
+  )
 }
 
 const App = () => {
@@ -29,18 +61,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [responses, addResponse] = useState([])
-
-  //stats functions (idk if JS has these built in??)
-  const sum = (arr) => {
-    return arr.reduce((total, current) => {
-        return total + current;
-    }, 0);
-  }
-  const percentPositive = (arr) => {
-    return(
-      100*(arr.filter(entry => entry === 1).length/arr.length) + ' %'
-    )
-  }
 
   //event handlers
   const clickGood = () => {
@@ -64,12 +84,8 @@ const App = () => {
       <Button click={clickNeutral} text='neutral'/>
       <Button click={clickBad} text='bad'/>
       <Title text='statistics' />
-      <Statistics text='good ' stat={good} />
-      <Statistics text='neutral ' stat={neutral} />
-      <Statistics text='bad ' stat={bad} />
-      <Statistics text='all ' stat={responses.length} />
-      <Statistics text='average ' stat={sum(responses)/responses.length} />
-      <Statistics text='positive ' stat={percentPositive(responses)} />
+      <Statistics responses={responses} />
+  
     </div>
   )
 }
