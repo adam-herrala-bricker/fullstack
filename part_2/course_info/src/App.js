435c06171b84ade511b023c53b88a7e3fb3e5818
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-07-31 11:03:47 +0300

    ex. 2.2-3

--- part_2/course_info/src/App.js
+++ part_2/course_info/src/App.js
@@ -1,3 +1,15 @@
+
+//function for sum of elements in array
+const sum = (arr) => arr.reduce((total, current) => total + current, 0)
+
+//function for pulling out exercise numbers from course object
+const exerciseTotaler = (course) => {
+  const exArr = course.parts.map((part) => part.exercises)
+  return(sum(exArr))
+
+}
+
+
 const Course = ({course}) => {
   return( 
     <div>
@@ -5,11 +17,10 @@ const Course = ({course}) => {
       <ul>
         {course.parts.map((part) => <li key = {part.id}>{part.name} {part.exercises}</li>)}
       </ul>
+      <h4>total of {exerciseTotaler(course)} exercises</h4>
     </div>
   )
 }
-
-
 
 const App = () => {
   const course = {
