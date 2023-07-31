3432808b1254798e22b8c8315177782bf0903fc7
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-07-31 14:38:42 +0300

    ex. 2.8

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -5,7 +5,7 @@ const Entry = ({persons}) => {
     <ul>
       {persons.map((person) =>
       <li key={person.name}>
-        {person.name}
+        {person.name} {person.number}
       </li>
       )}
     </ul>
@@ -13,20 +13,22 @@ const Entry = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas' }]) 
-  const [newName, setNewName] = useState('. . .')
+  const [persons, setPersons] = useState([ { name: 'Arto Hellas', number: 'KL5-3226' }]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] =useState ('')
 
   //Event handlers
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
       name: newName,
-      number: '(616) 844-2540'
+      number: newNumber
     }
     //check for repeat entry
     //NOTE THE SYNTAX!! This is far more temperamental than, say, python.
     // Can't check for .includes({object}) bc of how object identity works:
     //e.g., [{name: 'Adam'}].includes({name='Adam'}) returns false!!!
+    //(also: allows for multiple people to share a number, since instructions didn't say otherwise)
     if (persons.map(i => i.name).includes(newPerson.name)) {
       //Also note the formatting here for a string w a variable
        window.alert(`${newName} is already added to phonebook`)
@@ -34,12 +36,13 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
    
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  
 
   return (
     <div>
@@ -47,6 +50,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
