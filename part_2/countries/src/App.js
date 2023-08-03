e089342dcc80aceeb87f70010be5481d95c03838
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-03 13:30:52 +0300

    ex. 2.20

--- part_2/countries/src/App.js
+++ part_2/countries/src/App.js
@@ -25,12 +25,12 @@ const InfoCard = ({country}) => {
         </li>
         )}
       </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
+      <img src={country.flags.png} alt={country.flags.alt} className='flagStyle'/>
     </div>
   )
 }
 
-const DisplaySearchResults = ({searchResults, searchTerm, countryData, singleCountryData, handleShowButton}) => {
+const DisplaySearchResults = ({searchResults, searchTerm, countryData, handleShowButton, weatherData}) => {
   //null on first render + display nothing when nothing searched
   if (!searchResults | searchTerm === '') {
     return(null)
@@ -61,29 +61,54 @@ const DisplaySearchResults = ({searchResults, searchTerm, countryData, singleCou
   if (searchResults.length === 1) {
     const countryName = searchResults[0].toLowerCase()
     const thisCountry = countryData.filter(i => i.name.common.toLowerCase() === countryName)[0]
+    if (!weatherData){
+      return(
+        <div>
+           <InfoCard country={thisCountry} />
+          <p>no weather information for {thisCountry.capital} found</p>
+        </div>
+    )
+  }
     return(
-      <InfoCard country={thisCountry} />
+      <div>
+        <InfoCard country={thisCountry} />
+        <WeatherCard country={thisCountry} weatherData={weatherData}/>
+      </div>
     )
   }
 }
 
-const Search = ({searchResults, searchTerm, handleSearchTermChange, countryData, handleShowButton, singleCountryData}) => {
+const Search = ({searchResults, searchTerm, handleSearchTermChange, countryData, handleShowButton, weatherData}) => {
   return(
     <div>
       find countries <input values = {searchTerm} onChange = {handleSearchTermChange}/>
-      <DisplaySearchResults searchResults = {searchResults} searchTerm = {searchTerm} countryData = {countryData} singleCountryData = {singleCountryData} handleShowButton = {handleShowButton}/>
+      <DisplaySearchResults searchResults = {searchResults} searchTerm = {searchTerm} countryData = {countryData} handleShowButton = {handleShowButton} weatherData ={weatherData}/>
     </div>
   )
 }
 
+const WeatherCard = ({country, weatherData}) => {
+  console.log(weatherData)
+  const iconURL = `https://openweathermap.org/img/wn/${weatherData.weather['0'].icon}@2x.png`
+  const thisCapital = country.capital
+  return(
+    <div>
+      <h1>Weather in {thisCapital}</h1>
+      <p>temperature {Math.round(weatherData.main.temp)} <sup>o</sup>C</p> 
+      <img src={iconURL} />
+      <p>wind {weatherData.wind.speed} m/s</p>
+    </div>
+  )
+
+}
+
 const App = () => {
   //states
-  const [currentCountry, setCurrentCountry] = useState(null) //idk if this gets used . . .
   const [countryData, setCountryData] = useState(null) //data for every country
-  const [singleCountryData, setSingleCountryData] = useState(null)
-  const [countryNames, setCountryNames] = useState([])
+  const [countryNames, setCountryNames] = useState([]) //set once; array of every country name
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResults, setSearchResults] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
 
   //hook to get array of country names
   const hook = () => {
@@ -100,36 +125,42 @@ const App = () => {
 
   useEffect(hook, [])
 
+  //helper function for fetching new weather data
+  const fetchWeatherData = (newCountryData) => {
+    const thisCity = newCountryData.capital
+    serverBuddy
+    .getWeather(thisCity)
+    .then(returnedData => {
+      setWeatherData(returnedData)
+    })
+    .catch(error => {
+      setWeatherData(null) //shockingly few aren't found. Palau is a good test.
+    })
+  }
+
   //event handlers
   const handleSearchTermChange = (event) => {
-    const currentSearchTerm = event.target.value
+    const currentSearchTerm = event.target.value.trim() //slightly more robust search if leading/trailing spaces don't break it
     setSearchTerm(currentSearchTerm)
     const newSearchResults = countryNames.filter(i => i.toLowerCase().includes(currentSearchTerm.toLowerCase()))
     setSearchResults(newSearchResults)
+    //get weather if exactly one result
+    if (newSearchResults.length === 1){
+      const countryName = newSearchResults[0]
+      const newCountryData = countryData.filter(i => i.name.common.toLowerCase() === countryName.toLowerCase())[0]
+      fetchWeatherData(newCountryData)
+    }
   }
 
   const handleShowButton = (countryName) => {
     const newCountryData = countryData.filter(i => i.name.common.toLowerCase() === countryName.toLowerCase())[0]
-    setSingleCountryData(newCountryData)
     setSearchResults([countryName]) //automatically triggers the info card
+    fetchWeatherData(newCountryData)
   }
-
-
-  //need something to get data from server
-  //note, don't think you use this, or the associated serve buddy thing, in the final product
-  const handleNewCountry = (country) => {
-    setCurrentCountry(country)
-    serverBuddy
-    .getCountry(country)
-    .then(returnedData => {
-      setCountryData(returnedData)
-    })
-  } 
-
   
   return(
     <>
-    <Search searchResults ={searchResults} searchTerm ={searchTerm} handleSearchTermChange = {handleSearchTermChange} handleShowButton = {handleShowButton} countryData = {countryData} singleCountryData = {singleCountryData}/>
+    <Search searchResults ={searchResults} searchTerm ={searchTerm} handleSearchTermChange = {handleSearchTermChange} handleShowButton = {handleShowButton} countryData = {countryData} weatherData={weatherData}/>
     </>
   )
 }
--- part_2/countries/src/services/serverKamu.js
+++ part_2/countries/src/services/serverKamu.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
 
 const baseURL = 'https://studies.cs.helsinki.fi/restcountries/api/'
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = process.env.REACT_APP_API_KEY //API key is in .env file in root directory
 
 const getAllCountries = () => {
     const request = axios.get(`${baseURL}all`)
     return(request.then(response => response.data))
 }
 
-const getCountry = (country) => {
-    const request = axios.get(`${baseURL}name/${country}`)
+const getWeather = (city) => {
+    const request = axios.get(`${weatherURL}${city}&appid=${apiKey}&units=metric`) //remarkably robust just giving it a city name
     return(request.then(response => response.data))
 }
 
-export default {getCountry, getAllCountries}
+export default {getAllCountries, getWeather}
