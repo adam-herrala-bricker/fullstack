615ae02d0470ffcf9182c4487ffca6bccecc5553
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-08 19:11:38 +0300

    ex. 3.13

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Entry = require('./models/entry')
 
 //middleware!!
 app.use(express.json())
@@ -16,29 +18,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
     }
     return(' ') 
 })
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 //helper function (gives big, pseudo-random number )
 const generateID = () => {
@@ -57,7 +36,9 @@ app.get('/info', (request, response)=> {
 
 //get request for all persons
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Entry.find({}).then(entries => {
+        response.json(entries)
+    })
 })
 
 //get request for single person
---
+++ part_3/phonebook_backend/models/entry.js
@@ -0,0 +1,31 @@
+//this uses mongoose to set a schema to deal w the database
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const entrySchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    })
+
+entrySchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__V
+    }
+})
+
+module.exports = mongoose.model('Entry', entrySchema)
