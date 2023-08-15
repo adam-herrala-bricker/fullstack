8f9bfc7917d98f584d77eeba7d4ba02f4dc2bd6f
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-15 11:23:10 +0300

    ex. 4.2

---
+++ part_4/blog/app.js
@@ -0,0 +1,33 @@
+//note: added stuff from the exercises too
+const {mongourl, PORT} = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', mongourl)
+
+mongoose.connect(mongourl)
+    .then(() => {
+        logger.info('connected to MongoDB!')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB: error.message')
+    })
+
+app.use(cors())
+app.use(express.json())
+/* would put connection to static FE and custom middleware here too, e.g.:
+app.use(express.static('build))
+app.use(middleWare.requestLogger)
+*/
+
+app.use('/api/blogs', blogsRouter)
+
+//then custom error handling MW that goes at end would go here
+
+module.exports = app
