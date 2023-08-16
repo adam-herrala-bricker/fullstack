fe79024a2ffadcdf93d0ba5b66a4848cfa3d8b5b
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-16 11:06:12 +0300

    ex. 4.8

---
+++ part_4/blog/tests/blog_api.test.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+    await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are 0 blogs', async () => {
+    const response = await api.get('/api/notes')
+    expect(response.body).toEqual({})
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+  })
--- part_4/blog/utils/config.js
+++ part_4/blog/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const mongourl = process.env.MONGODB_URI
+const mongourl = process.env.NODE_ENV === 'testing'
+    ? process.env.TEST_MONGODB_URI
+    : processes.env.MONGODB_URI
 
 module.exports = {mongourl, PORT}
