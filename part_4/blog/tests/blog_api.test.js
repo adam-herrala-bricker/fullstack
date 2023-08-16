f1e13fd90b69009b0e14c2d73fc0accb5d9c65fe
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-16 11:35:20 +0300

    ex. 4.9

--- part_4/blog/tests/blog_api.test.js
+++ part_4/blog/tests/blog_api.test.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+//testing from the same state every time
+const initialBlogs = [
+    {
+        title: 'every blog, ranked',
+        author: "Matt Matthews",
+        url: "deadspin.com/blog-ranked",
+        likes : 10,
+    },
+    {
+        title: 'every dog, ranked',
+        author: 'Dave Davies',
+        url: 'deadspin.com/dog-ranked',
+        likes: 40,
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
     await api.get('/api/blogs')
@@ -11,8 +36,13 @@ test('blogs are returned as json', async () => {
 })
 
 test('there are 0 blogs', async () => {
-    const response = await api.get('/api/notes')
-    expect(response.body).toEqual({})
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+test.only('contains id property', async() => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0]['id']).toBeDefined
 })
 
 afterAll(async () => {
