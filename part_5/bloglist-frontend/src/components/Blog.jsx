4a1d38ec7f4a0827076b63748f9806df737420d6
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-21 15:44:48 +0300

    ex. 5.7

--- part_5/bloglist-frontend/src/components/Blog.jsx
+++ part_5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} -- {blog.author}
-  </div>  
-)
+import { useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [unroll, setUnroll] = useState(false)
+
+  //event handlers
+  const toggleUnroll = () => {
+    setUnroll(!unroll)
+  }
+  
+  if (unroll) {
+    return(
+      <div className = 'bloggy'>
+        <p>{blog.title} -- {blog.author} <button onClick={toggleUnroll}>hide</button></p>
+        <p>{blog.url}</p>
+        <p>likes: {blog.likes} <button>like</button></p>
+      </div>
+    )
+  }
+  
+  
+  return(
+    <div className = 'bloggy'>
+    {blog.title} -- {blog.author} <button onClick={toggleUnroll}>view</button>
+  </div> 
+  )
+   
+}
 
 export default Blog
