242415404e965364b5f7d177ac76258f85754972
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-22 11:34:06 +0300

    ex. 5.14

--- part_5/bloglist-frontend/src/tests/Blog.test.js
+++ part_5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
-test('renders content', () => {
-    const blog = {
-        title: 'test title',
-        author: 'test author',
-        url: 'test.com/url',
-        likes: 17,
-        user: {
-            username: 't.ester',
-            name: 'Tester Randy',
-            id: 'tttttttttttttttttttttttt'
-        }
+const blog = {
+    title: 'test title',
+    author: 'test author',
+    url: 'test.com/url',
+    likes: 17,
+    user: {
+        username: 't.ester',
+        name: 'Tester Randy',
+        id: 'tttttttttttttttttttttttt'
     }
+}
 
+const user0 = {
+    username: 't.ester',
+    name: 'Tester Randy'
+}
+
+
+const user1 = {
+    username: 'randy_15',
+    name: 'Password Randy'
+}
+
+test('renders default content', () => {
+
+    //renders the component we're interested in testing
     render(<Blog blog= {blog}/>)
 
     //renders title by default
@@ -30,4 +44,21 @@ test('renders content', () => {
 
     const url = screen.queryByText('17', {exact: false})
     expect(url).toBeNull
+})
+
+test('renders full content on click', async () => {
+    //note: don't need to make mock event handler here bc the toggleUnroll EH is inside the Blog component
+
+    render(<Blog blog = {blog} user = {user0}/>)
+
+    //start session to interact with rendered component
+    const user = userEvent.setup()
+
+    const button = screen.getByText('view')
+
+    await user.click(button)
+
+    //likes and URL are now rendered
+    screen.getByText('test.com/url', {exact: false})
+    screen.getByText('17', {exact: false})
 })
