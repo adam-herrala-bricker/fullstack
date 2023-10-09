7b9d23a3a3be7821fd6b981d4b09695613f250b4
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-10-09 17:30:01 +0300

    ex. 9.12

--- part_9/patientor/backend/routes/patientsRoutes.ts
+++ part_9/patientor/backend/routes/patientsRoutes.ts
@@ -1,11 +1,17 @@
 import express from 'express';
-import { getPatientsNonsensitive } from '../services/patientsServices';
+import { getPatientsNonsensitive, addPatient } from '../services/patientsServices';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
     res.send(getPatientsNonsensitive());
 
+});
+
+router.post('/', (req, res) => {
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body
+    const addedEntry = addPatient(name, dateOfBirth, ssn, gender, occupation);
+    res.send(addedEntry);
 });
 
 export default router;
--- part_9/patientor/backend/services/patientsServices.ts
+++ part_9/patientor/backend/services/patientsServices.ts
@@ -1,16 +1,35 @@
 import patientsData from '../../data/patients';
+import { v1 as uuid } from 'uuid';
 import { Patient, NonSensitivePatientData } from '../types';
+
+const patients = patientsData;
 
 //not actually used in app. here for illustration only
 export const getPatients = (): Patient[] => {
-    return patientsData;
+    return patients;
 };
 
 export const getPatientsNonsensitive = (): NonSensitivePatientData[] => {
-    return patientsData.map(({id, name, dateOfBirth, gender, occupation}) => ({
+    return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
 
         id, name, dateOfBirth, gender, occupation
     }));
+};
+
+export const addPatient = (name: string, dateOfBirth: string, ssn: string, gender: string, occupation: string): Patient => {
+    const newPatient: Patient = {
+        id : uuid(),
+        name,
+        dateOfBirth,
+        ssn,
+        gender,
+        occupation
+    };
+
+    patients.push(newPatient);
+
+    return newPatient;
+
 };
 
 
