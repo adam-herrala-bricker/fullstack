2b5cb309c36b2c057e5ce655ae6042579d6cca21
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-10-26 12:45:54 +0300

    ex. 10.4

---
+++ part_10/rate-repo-app/src/components/AppBar.jsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet, Pressable, Text } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    marginBottom: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 3,
+    backgroundColor: theme.colors.backgroundDark,
+  },
+  text: {
+    color: theme.colors.textWhite,
+    fontSize: theme.fontSizes.heading,
+    fontWeight: theme.fontWeights.bold
+  }
+});
+
+const AppBar = () => {
+  return (
+  <View style={styles.container}>
+    <Pressable>
+        <Text style = {styles.text}>
+            Repositories
+        </Text>
+    </Pressable>
+</View>
+  );
+};
+
+export default AppBar;
---
+++ part_10/rate-repo-app/src/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    colors: {
+        backgroundDark: '#24292e',
+        textWhite: '#FFFFFF'
+    },
+    fontSizes: {
+        heading: 20
+    },
+    fontWeights: {
+        bold: '700'
+    }
+};
+
+export default theme;
