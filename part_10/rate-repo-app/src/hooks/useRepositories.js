f481f4ced40cbcfc89e21e20037718b1f4ad6646
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-11-01 12:12:20 +0200

    ex. 10.12

--- part_10/rate-repo-app/src/hooks/useRepositories.js
+++ part_10/rate-repo-app/src/hooks/useRepositories.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
     const queryResponse = useQuery(GET_REPOSITORIES, {fetchPolicy: 'cache-and-network'})
+   
     //console.log(queryResponse.data.repositories.edges)
 
     //return { repositories, loading, refetch: fetchRepositories };
     //note that this doesn't have a refetch function now, but unclear if it needs one?
     return { 
-                repositories: queryResponse.data.repositories,
+                repositories: queryResponse.data?.repositories,
                 loading: queryResponse.loading
 
             }
